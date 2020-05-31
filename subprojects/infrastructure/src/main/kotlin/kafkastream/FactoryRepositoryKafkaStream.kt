ba54a59e7527e9fa89b2e319f91e459e0e99cc9e
tatitati <francis.albusac@simplybusiness.co.uk>
2020-05-31 21:23:22 +0100

    Sun May 31 21:23:22 BST 2020

--- subprojects/infrastructure/src/main/kotlin/kafkastream/FactoryRepositoryKafkaStream.kt
+++ subprojects/infrastructure/src/main/kotlin/kafkastream/FactoryRepositoryKafkaStream.kt
@@ -18,10 +18,12 @@ import java.util.*
 class FactoryRepositoryKafkaStream(
         @Value ("\${spring.kafka.bootstrap-servers}") private val bootstrapServers: String,
         @Value ("\${spring.kafka.kstreams.input-topic}") private val inputTopic: String,
-        @Value ("\${spring.kafka.kstreams.output-topic}") private val outputTopic: String
+        @Value ("\${spring.kafka.kstreams.output-topic}") private val outputTopic: String,
+        @Value ("\${spring.kafka.kstreams.input-topic2}") private val inputTopic2: String,
+        @Value ("\${spring.kafka.kstreams.output-topic2}") private val outputTopic2: String
 ) {
     @Bean
-    fun repositoryKafkaStream(): RepositoryKStreams {
+    fun singleKafkaStream(): RepositoryKStreams {
         val properties = Properties()
         properties.apply{
             put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-application")
@@ -44,9 +46,55 @@ class FactoryRepositoryKafkaStream(
                     println(splitText)
                     splitText
                 }
-
-
+        
         processedStream.to(outputTopic, Produced.with(Serdes.String(), Serdes.String()))
+
+        val topology: Topology = streamsBuilder.build()
+        val streams: KafkaStreams = KafkaStreams(topology, properties)
+
+        return RepositoryKStreams(streams)
+    }
+
+    @Bean
+    fun multiKafkaStreams(): RepositoryKStreams {
+        val properties = Properties()
+        properties.apply{
+            put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-application")
+            put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+            put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+        }
+
+        val streamsBuilder = StreamsBuilder()
+
+        // kafkastream app 1:
+        streamsBuilder
+                .stream(inputTopic, Consumed.with(Serdes.String(), Serdes.String()))
+                .mapValues { textLine ->
+                    textLine.toLowerCase()
+                }
+                .flatMapValues { loweredCase ->
+                    loweredCase.split(" ")
+                }
+                .mapValues { splitText ->
+                    println("app1: $splitText")
+                    "app1: $splitText"
+                }
+                .to(outputTopic, Produced.with(Serdes.String(), Serdes.String()))
+
+        // kafkastream app 2:
+        streamsBuilder
+                .stream(inputTopic2, Consumed.with(Serdes.String(), Serdes.String()))
+                .mapValues { textLine ->
+                    textLine.toLowerCase()
+                }
+                .flatMapValues { loweredCase ->
+                    loweredCase.split(" ")
+                }
+                .mapValues { splitText ->
+                    println("app2: $splitText")
+                    "app2: $splitText"
+                }
+                .to(outputTopic2, Produced.with(Serdes.String(), Serdes.String()))
 
         val topology: Topology = streamsBuilder.build()
         val streams: KafkaStreams = KafkaStreams(topology, properties)
