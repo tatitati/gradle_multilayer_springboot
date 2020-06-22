c720b0fbf5517d34d7c7fb4a1428fa67714d8e5d
tatitati <francis.albusac@simplybusiness.co.uk>
2020-06-23 00:00:08 +0100

    first code of producer with batchin

--- subprojects/infrastructure/src/test/kotlin/ProducerBatchingTest.kt
+++ subprojects/infrastructure/src/test/kotlin/ProducerBatchingTest.kt
@@ -1,4 +1,20 @@
 package myapp.test.infrastructure
 
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import java.util.*
+
 class ProducerBatchingTest {
+
+    fun buildProducer(): KafkaProducer<String, String>{
+        val properties = Properties().apply{
+            put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "")
+            put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "")
+            put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "")
+        }
+
+        return KafkaProducer<String, String>(
+                properties
+        )
+    }
 }
