61bba4227fa3561650741e18ed7741d81553f9ec
tatitati <francis.albusac@simplybusiness.co.uk>
2020-07-07 22:23:06 +0100

    adsf

--- subprojects/infrastructure/src/test/kotlin/producer/serializers/JsonSerializerSchemaTest.kt
+++ subprojects/infrastructure/src/test/kotlin/producer/serializers/JsonSerializerSchemaTest.kt
@@ -2,6 +2,7 @@ package myapp.test.infrastructure.producer.serializers
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.confluent.kafka.serializers.KafkaJsonSerializer
+import io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericRecord
 import org.apache.avro.generic.GenericRecordBuilder
@@ -13,7 +14,7 @@ import java.util.*
 
 class JsonSerializerSchemaTest {
 
-    class Book(val myField1: Int, val myField2: Double, myField3: String)
+    data class Book(val myField1: Int, val myField2: Double, val myField3: String)
 
     fun buildProducer(): KafkaProducer<String, Book> {
         val properties = Properties().apply{
@@ -28,38 +29,43 @@ class JsonSerializerSchemaTest {
 
     @Test
     fun jsonProducer(){
-        val sche = "\$schema"
-        val id = "\$id"
-        val schema = Schema.Parser().parse("""
-            {
-              "$sche": "http://json-schema.org/draft-07/schema#",
-              "$id": "http://example.com/myURI.schema.json",
-              "title": "value_jsons_serializer_schemaless",
-              "description": "Sample schema to help you get started.",
-              "type": "object",
-              "properties": {
-                "myField1": {
-                  "type": "integer",
-                  "description": "The integer type is used for integral numbers."
-                },
-                "myField2": {
-                  "type": "number",
-                  "description": "The number type is used for any numeric type, either integers or floating point numbers."
-                },
-                "myField3": {
-                  "type": "string",
-                  "description": "The string type is used for strings of text."
-                }
-              }
-            }
-        """.trimIndent())
-
-
         val book = Book(myField1 = 13, myField2 = 46.8, myField3 = "some text here")
         buildProducer().apply{
-            send(ProducerRecord("topic-jsonserializer-schema", book))
+            send(ProducerRecord("topic-schema-jsonserializer", book))
             flush()
             close()
         }
     }
+
+    // Without passing any schema this creates the next schema:
+    // ========================================================
+    //    {
+    //        "$schema": "http://json-schema.org/draft-04/schema#",
+    //        "additionalProperties": false,
+    //        "properties": {
+    //        "myField1": {
+    //        "type": "integer"
+    //    },
+    //        "myField2": {
+    //        "type": "number"
+    //    },
+    //        "myField3": {
+    //        "oneOf": [
+    //        {
+    //            "title": "Not included",
+    //            "type": "null"
+    //        },
+    //        {
+    //            "type": "string"
+    //        }
+    //        ]
+    //    }
+    //    },
+    //        "required": [
+    //        "myField1",
+    //        "myField2"
+    //        ],
+    //        "title": "Book",
+    //        "type": "object"
+    //    }
 }
