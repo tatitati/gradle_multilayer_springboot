b10a350a95eeb56656ea2d7173fcf9115899397d
tatitati <francis.albusac@simplybusiness.co.uk>
2020-07-08 11:29:22 +0100

    adsfadsf

---
+++ subprojects/infrastructure/src/test/kotlin/schemaregistry/MaidaValidator.kt
@@ -0,0 +1,47 @@
+package myapp.test.infrastructure.schemaregistry
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.worldturner.medeia.api.JsonSchemaVersion
+import com.worldturner.medeia.api.UrlSchemaSource
+import com.worldturner.medeia.api.jackson.MedeiaJacksonApi
+import com.worldturner.medeia.schema.validation.SchemaValidator
+import org.junit.jupiter.api.Test
+import java.io.StringWriter
+import java.net.URL
+
+data  class Mydata(val whatever: String)
+
+class SchemaResolutionTest {
+    private val api = MedeiaJacksonApi()
+    private val objectMapper = jacksonObjectMapper()
+
+    @Test
+    fun loadSchemaFromFileSystem(): SchemaValidator {
+        val source = UrlSchemaSource(
+                url = URL("http://localhost:8081/schemas/ids/4"),
+                version = JsonSchemaVersion.DRAFT07
+        )
+        val schema = api.loadSchemas(listOf(source))
+        return schema
+    }
+
+    @Test
+    fun loadSchemaOriginal(): SchemaValidator {
+        val source = UrlSchemaSource(
+                javaClass.getResource("/myschemas/with-default-schema.json")
+        )
+        val schema = api.loadSchema(source)
+        return schema
+    }
+
+    @Test
+    fun whatever(){
+        val validator = loadSchemaFromFileSystem()
+        val s = StringWriter()
+        val unvalidatedGenerator = objectMapper.factory.createGenerator(s)
+        val validatedGenerator = api.decorateJsonGenerator(validator, unvalidatedGenerator)
+        objectMapper.writeValue(validatedGenerator, Mydata("gfdgdfsg"))
+        println(s)
+    }
+
+}
