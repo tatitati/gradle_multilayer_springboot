f256bf160c89912af2658dcd3fd4a3f677c289f7
tatitati <francis.albusac@simplybusiness.co.uk>
2020-07-08 11:05:26 +0100

    asdf

--- subprojects/infrastructure/src/test/kotlin/producer/serializers/JsonSerializerCustomTest.kt
+++ subprojects/infrastructure/src/test/kotlin/producer/serializers/JsonSerializerCustomTest.kt
@@ -1,11 +1,20 @@
 package myapp.test.infrastructure.producer.serializers
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import io.confluent.kafka.serializers.KafkaJsonSerializer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.IntegerSerializer
 import org.apache.kafka.common.serialization.Serializer
+import org.apache.kafka.common.serialization.StringSerializer
 import org.junit.jupiter.api.Test
+import java.io.IOException
+import java.io.StringWriter
 import java.util.*
 
 data class User(val firstName: String, val lastName: String, val age: Int)
@@ -14,7 +23,13 @@ class UserSerializer: Serializer<User> {
     override fun configure(map: Map<String?, *>?, b: Boolean) {}
     override fun close() {}
     override fun serialize(arg0: String?, arg1: User?): ByteArray? {
-        return ObjectMapper().writeValueAsString(arg1).toByteArray()
+        val data = """
+            {
+                "firstName":"${arg1!!.firstName}",
+                "lastName":"${arg1!!.lastName}",
+                "age":${arg1!!.age}
+            }""".trimIndent()
+        return data.toByteArray()
     }
 }
 
@@ -33,10 +48,10 @@ class JsonProducerTest {
 
     @Test
     fun jsonProducer(){
-        val user = User(firstName = "namehere", lastName = "lastname here", age = (0 until 10000).random())
+        val user = User(firstName = "anotherone", lastName = "lastname here", age = (0 until 10000).random())
 
         buildProducer().apply{
-            send(ProducerRecord("topic-custom-serializer", user))
+            send(ProducerRecord("topic-custom-serializer7", user)).get()
             flush()
             close()
         }
