2fef002a7d3d972f49b479a29cfd29ff35b917ee
tatitati <francis.albusac@simplybusiness.co.uk>
2020-07-10 13:17:19 +0100

    asd

--- subprojects/infrastructure/src/test/kotlin/schemaregistry/SchemaRegistryClientTest.kt
+++ subprojects/infrastructure/src/test/kotlin/schemaregistry/SchemaRegistryClientTest.kt
@@ -13,25 +13,12 @@ class SchemaRegistryClientTest {
     fun basictest(){
         val client = CachedSchemaRegistryClient("http://127.0.0.1:8081", 1000)
 
-//        val allSubjects = client.allSubjects
-//        println(allSubjects) // [mydraft7-value, topic-kafkajsonschemaserializer-value, topic-kafkajsonschemaserializer-value]
-//
-//        val allSchemaVersions = client.getAllVersions("mydraft7-value")
-//        println(allSchemaVersions) // [1, 2]
-//
-//        val mode1 = client.getAllSubjectsById(1)
-//        val mode2 = client.getAllSubjectsById(2)
-//        val mode3 = client.getAllSubjectsById(3)
-//        println(mode1) // [mydraft7-value, topic-kafkajsonschemaserializer-value]
-//        println(mode2) // [mydraft7-value]
-//        println(mode3) // [topic-withavrorecord-schema-value]
-//
-//        val avroSchema = client.getSchemaById(3) // {"type":"record","name":"value_jsons_serializer_schemaless","namespace":"com.mycorp.mynamespace","doc":"Sample schema to help you get started.","fields":[{"name":"myField1","type":"int","doc":"The int type is a 32-bit signed integer."},{"name":"myField2","type":"double","doc":"The double type is a double precision (64-bit) IEEE 754 floating-point number."},{"name":"myField3","type":"string","doc":"The string is a unicode character sequence."}]}
-//        println(avroSchema)
-
-
-//        val jsonSchema = client.getSchemaById(2) // {"type":"record","name":"value_jsons_serializer_schemaless","namespace":"com.mycorp.mynamespace","doc":"Sample schema to help you get started.","fields":[{"name":"myField1","type":"int","doc":"The int type is a 32-bit signed integer."},{"name":"myField2","type":"double","doc":"The double type is a double precision (64-bit) IEEE 754 floating-point number."},{"name":"myField3","type":"string","doc":"The string is a unicode character sequence."}]}
-//        println(jsonSchema)
+        val allSubjects = client.allSubjects
+        val allSchemaVersions = client.getAllVersions("mydraft7-value")
+        val jsonSchema = client.getSchemaById(2)
+        val schemaVersion = client.getByVersion("mydraft7-value", 1, false)
+        println(schemaVersion)
+        jsonSchema.validate()
 
 
         val sche = "\$schema"
@@ -63,13 +50,8 @@ class SchemaRegistryClientTest {
 
         val schemaObject = JSONObject(JSONTokener(rawSchemaEvolution))
         val schemaLoaded = SchemaLoader.load(schemaObject)
-
         val schema = JsonSchema(schemaLoaded)
-
         schema.validate()
-        println(schema.schemaType()) // JSON
-        println(schema.references()) // []
-        println(schema.version())
         val result = client.register("mynewsubject", schema)
         println(result) // 12
 
