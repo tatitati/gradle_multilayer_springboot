a7bdcb18af2c0127379954b9fda199dd22eb306a
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-03 22:21:24 +0100

    added idempotent producer, using transactions

--- subprojects/infrastructure/src/test/kotlin/producer/ProducerIdempotentTest.kt
+++ subprojects/infrastructure/src/test/kotlin/producer/ProducerIdempotentTest.kt
@@ -3,6 +3,7 @@ package myapp.test.infrastructure.producer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.KafkaException
 import org.junit.jupiter.api.Test
 import java.util.*
 
@@ -10,14 +11,13 @@ class ProducerIdempotentTest {
 
     fun buildProducer(): KafkaProducer<String, String>{
         val properties = Properties().apply{
-            put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094")
+            put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
             put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerSerializer")
             put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
             // params for safe producer
             put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true")
-            put(ProducerConfig.ACKS_CONFIG, "all")
-            put(ProducerConfig.RETRIES_CONFIG, Int.MAX_VALUE.toString())
-            put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5")
+            put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "my_idempotent_producer");
+
         }
 
         return KafkaProducer<String, String>(
@@ -26,13 +26,36 @@ class ProducerIdempotentTest {
     }
 
     @Test
-    fun testBatching(){
-        val producer = buildProducer()
-        val msgs = arrayOf("ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN")
+    fun testIdempotentProducer(){
+        val msgs = arrayOf("ONE", "TWO", "THREE", "FOUR")
 
-        msgs.forEach{ msg ->
-            producer.send(
-                    ProducerRecord("topic-ProducerIdempotentTest", msg))
+        val producer = buildProducer()
+
+        producer.initTransactions()
+        try {
+            msgs.forEach{ msg ->
+                producer.beginTransaction()
+                producer.send(ProducerRecord<String, String>("topic-ProducerIdempotentTest", msg))
+                producer.commitTransaction();
+            }
+
+        } catch (e: KafkaException){
+            print ("exception")
+            producer.abortTransaction();
+        }
+
+        // NOW IN ANOTHER TRANSACTION WE TRY again the same, but these messages won't be sent as they are duplicates :)
+        producer.initTransactions()
+        try {
+            msgs.forEach{ msg ->
+                producer.beginTransaction()
+                producer.send(ProducerRecord<String, String>("topic-ProducerIdempotentTest", msg))
+                producer.commitTransaction();
+            }
+
+        } catch (e: KafkaException){
+            print ("exception")
+            producer.abortTransaction();
         }
 
         producer.apply{
