5e6586e7a35f1d2a57d7302f41b528c2a5772f2a
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-10 00:15:26 +0100

    more

--- subprojects/infrastructure/src/test/kotlin/consumer/CommitOffset/ManualCommitTest.kt
+++ subprojects/infrastructure/src/test/kotlin/consumer/CommitOffset/ManualCommitTest.kt
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.junit.jupiter.api.Test
 import java.time.Duration
+import java.time.LocalDateTime
 import java.util.*
 
 class ManualCommitTest {
@@ -14,7 +15,7 @@ class ManualCommitTest {
             put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094")
             put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer")
             put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
-            put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group-test")
+            put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-manualcommitoffset")
             // manual commit of offsets
             put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
         }
@@ -33,13 +34,20 @@ class ManualCommitTest {
 
         while (true) {
             val batchOfRecords: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(20))
-            println("Received a batch with records amount: " + batchOfRecords.count())
+            println("\n\n\nReceived a batch with records amount: " + batchOfRecords.count())
 
             // process consumed batch records
             batchOfRecords.iterator().forEach {
-                println("=========> Partition: " + it.partition() + ", Offset: " + it.offset() + ", Key: " + it.key() + ", Value: " + it.value())
-                consumer.commitSync()
+                println("=========> Record:")
+                val current2 = LocalDateTime.now()
+                println("\t=========> Partition: " + it.partition() + ", Offset: " + it.offset() + ", Key: " + it.key() + ", Value: " + it.value())
+                Thread.sleep(5_000) // 10 secs
             }
+
+            // commitAsync commit the offset for all the records fetched. It doesnt make sense to put this
+            // inside the loop, because it doesn't commit record by record, byt the whole batch
+            consumer.commitSync()
+            println("\t=========> offset commited")
         }
     }
 }
