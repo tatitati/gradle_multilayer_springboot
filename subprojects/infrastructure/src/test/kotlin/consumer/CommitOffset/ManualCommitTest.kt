0a2582c5b75cf4dab35b54e8a99e39d26aa05eef
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-09 22:51:17 +0100

    renamed classes

--- subprojects/infrastructure/src/test/kotlin/consumer/CommitOffset/ManualCommitTest.kt
+++ subprojects/infrastructure/src/test/kotlin/consumer/CommitOffset/ManualCommitTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 import java.time.Duration
 import java.util.*
 
-class ConsumerAutoCommitOffsetTest {
+class ManualCommitTest {
 
     fun buildConsumer(): KafkaConsumer<String, String> {
         val properties = Properties().apply{
@@ -15,16 +15,15 @@ class ConsumerAutoCommitOffsetTest {
             put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer")
             put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
             put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group-test")
-            // automatic commit of offsets
-            put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+            // manual commit of offsets
+            put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
         }
 
         val consumer = KafkaConsumer<String, String>(
                 properties
         )
 
-        val topic = "consumer-group-topic"
-        consumer.subscribe(listOf(topic))
+        consumer.subscribe(listOf("topic-offset-manual"))
         return consumer
     }
 
@@ -34,8 +33,9 @@ class ConsumerAutoCommitOffsetTest {
 
         while (true) {
             val batchOfRecords: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(20))
-            println("Received a batch with recods amount: " + batchOfRecords.count())
+            println("Received a batch with records amount: " + batchOfRecords.count())
 
+            // process consumed batch records
             batchOfRecords.iterator().forEach {
                 println("=========> Partition: " + it.partition() + ", Offset: " + it.offset() + ", Key: " + it.key() + ", Value: " + it.value())
                 consumer.commitSync()
