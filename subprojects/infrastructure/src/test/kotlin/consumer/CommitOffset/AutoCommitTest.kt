f49fbcb6dc507a8ecf008e3a8e743b8008fa9f4d
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-10 00:15:48 +0100

    asdfasdf

--- subprojects/infrastructure/src/test/kotlin/consumer/CommitOffset/AutoCommitTest.kt
+++ subprojects/infrastructure/src/test/kotlin/consumer/CommitOffset/AutoCommitTest.kt
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.junit.jupiter.api.Test
 import java.time.Duration
+import java.time.LocalDateTime
 import java.util.*
 
 class AutoCommitTest {
@@ -14,9 +15,10 @@ class AutoCommitTest {
             put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094")
             put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer")
             put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
-            put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group-test")
+            put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-autocommitoffset")
             // automatic commit of offsets
-            put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+            put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true") // by default every 5 secs
+            put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "20000")
         }
 
         val consumer = KafkaConsumer<String, String>(
@@ -32,12 +34,16 @@ class AutoCommitTest {
         val consumer = buildConsumer()
 
         while (true) {
-            val batchOfRecords: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(20))
-            println("Received a batch with recods amount: " + batchOfRecords.count())
+            val current = LocalDateTime.now()
+            println("\n\n\n======> Time: $current")
+
+            val batchOfRecords: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(60))
+            val current2 = LocalDateTime.now()
+            println("======> Poll completed. Time: $current2")
+            println("======> Received a batch with recods amount: " + batchOfRecords.count())
 
             batchOfRecords.iterator().forEach {
-                println("=========> Partition: " + it.partition() + ", Offset: " + it.offset() + ", Key: " + it.key() + ", Value: " + it.value())
-                consumer.commitSync()
+                println("======> Partition: " + it.partition() + ", Offset: " + it.offset() + ", Key: " + it.key() + ", Value: " + it.value())
             }
         }
     }
