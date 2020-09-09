9a53445af947fba0ee801ba3a8c5d037db36944d
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-10 00:16:30 +0100

    more

--- subprojects/infrastructure/src/test/kotlin/consumer/CommitOffset/ManualCommitTest.kt
+++ subprojects/infrastructure/src/test/kotlin/consumer/CommitOffset/ManualCommitTest.kt
@@ -5,7 +5,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.junit.jupiter.api.Test
 import java.time.Duration
-import java.time.LocalDateTime
 import java.util.*
 
 class ManualCommitTest {
