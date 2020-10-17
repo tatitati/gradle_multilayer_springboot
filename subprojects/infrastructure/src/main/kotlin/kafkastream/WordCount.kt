c6492f6464e21c8fc3d2b7052ae79c73cde71720
tatitati <francis.albusac@simplybusiness.co.uk>
2020-10-18 00:53:15 +0100

    wordcount added

---
+++ subprojects/infrastructure/src/main/kotlin/kafkastream/WordCount.kt
@@ -0,0 +1,64 @@
+package myapp.infrastructure.kafkastream
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.Topology
+import org.apache.kafka.streams.kstream.*
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import java.util.*
+import javax.annotation.PostConstruct
+
+@SpringBootApplication
+class Main {
+    fun buildTopology(): Topology {
+        val builder = StreamsBuilder()
+
+        val wordCountInput = builder.stream("input_topic", Consumed.with(Serdes.String(), Serdes.String()))
+
+        val wordCountsResult = wordCountInput
+                .mapValues { textLine ->
+                    textLine.toLowerCase()
+                }
+                .flatMapValues { loweredCase ->
+                    loweredCase.split(" ")
+                }
+                .selectKey { key, word ->
+                    word
+                }
+                .groupByKey()
+                .count()
+
+        wordCountsResult.toStream().to("output_topic", Produced.with(Serdes.String(), Serdes.Long()))
+
+        val topology: Topology = builder.build()
+        return topology
+    }
+
+    @PostConstruct
+    fun run(){
+        val prop = Properties().apply {
+            put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-application")
+            put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+            put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String()::class.java)
+            put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String()::class.java)
+        }
+
+        val topology = buildTopology()
+        val streams = KafkaStreams(topology, prop)
+
+        streams.start()
+        Runtime.getRuntime().addShutdownHook(Thread(streams::close))
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<Main>(*args)
+
+    // CLI for consumer:
+    //    kafka-console-consumer --bootstrap-server $khost --topic output_topic --group mygroup --property  key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property  value.deserializer=org.apache.kafka.common.serialization.LongDeserializer --property print.key=true --property print.value=true
+}
