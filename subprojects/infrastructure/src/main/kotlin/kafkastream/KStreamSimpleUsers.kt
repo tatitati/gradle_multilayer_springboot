7cdf3264aefd1e41186e14ad5eab3776b60030c4
tatitati <francis.albusac@simplybusiness.co.uk>
2020-10-20 23:49:12 +0100

    a cool stream

--- subprojects/infrastructure/src/main/kotlin/kafkastream/KStreamSimpleUsers.kt
+++ subprojects/infrastructure/src/main/kotlin/kafkastream/KStreamSimpleUsers.kt
@@ -5,14 +5,18 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.util.StdDateFormat
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import myapp.infrastructure.kafkastream.pojos.Person
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.IntegerSerializer
 import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.serialization.StringSerializer
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.StreamsConfig
 import org.apache.kafka.streams.kstream.Consumed
 import org.apache.kafka.streams.kstream.KStream
-import org.apache.kafka.streams.kstream.Printed
 import org.apache.kafka.streams.kstream.Produced
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -24,20 +28,48 @@ import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class KStreamSimpleUsers {
-    val streamsBuilder = StreamsBuilder()
-    val props = Properties().apply {
-        put(StreamsConfig.APPLICATION_ID_CONFIG, "kstream-application")
-        put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-        put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String()::class.java)
-        put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String()::class.java)
+    val jsonMapper = ObjectMapper().apply {
+        registerKotlinModule()
+        disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        setDateFormat(StdDateFormat())
     }
+
+    fun sendToTopicSomeUsers(){
+        val properties = Properties().apply{
+            put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+            put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+            put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+        }
+
+        val producer = KafkaProducer<String, String>(properties)
+
+        for(i in 1..10) {
+            val person = Person(
+                    firstName = "firstname"+i,
+                    lastName = "lastName"+i,
+                    birthDate = Date(2020, 3, 15)
+            )
+            val futureResult = producer.send(ProducerRecord("topic-input-person", jsonMapper.writeValueAsString(person)))
+            futureResult.get()
+        }
+
+        producer.flush()
+        producer.close()
+    }
+
+
 
     @PostConstruct
     fun run(){
-        val jsonMapper = ObjectMapper().apply {
-            registerKotlinModule()
-            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            setDateFormat(StdDateFormat())
+        this.sendToTopicSomeUsers()
+
+
+        val streamsBuilder = StreamsBuilder()
+        val props = Properties().apply {
+            put(StreamsConfig.APPLICATION_ID_CONFIG, "kstream-application")
+            put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+            put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String()::class.java)
+            put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String()::class.java)
         }
 
         val personJsonStream: KStream<String, String> = streamsBuilder.stream<String, String>(
