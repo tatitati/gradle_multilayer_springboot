e4287977c85f752ea98ca52340c6a3865d694b1c
tatitati <francis.albusac@simplybusiness.co.uk>
2020-10-21 19:58:45 +0100

    more

--- subprojects/infrastructure/src/main/kotlin/kafkastream/store/MaterializedSimple.kt
+++ subprojects/infrastructure/src/main/kotlin/kafkastream/store/MaterializedSimple.kt
@@ -2,40 +2,69 @@ package myapp.infrastructure.kafkastream
 
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.kstream.*
 import org.apache.kafka.streams.kstream.Materialized
-import org.apache.kafka.streams.kstream.Suppressed.BufferConfig.unbounded
+import org.apache.kafka.streams.processor.ProcessorContext
+import org.apache.kafka.streams.state.KeyValueStore
+import org.apache.kafka.streams.state.StoreBuilder
+import org.apache.kafka.streams.state.Stores
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.time.Duration
 import java.util.*
 import javax.annotation.PostConstruct
 
+
+class MyTransformer: ValueTransformer<String, String>{
+    override fun init(context: ProcessorContext?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun transform(value: String?): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+
+}
+
 @SpringBootApplication
 class MaterializedSimple {
-    val prop: Properties = Properties().apply {
-        put(StreamsConfig.APPLICATION_ID_CONFIG, "materialized-application")
-        put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-        put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String()::class.java)
-        put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String()::class.java)
+
+    private fun getProps(): Properties {
+        return Properties().apply {
+            put(StreamsConfig.APPLICATION_ID_CONFIG, "materialized-application")
+            put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+            put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde())
+            put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.LongSerde())
+        }
     }
-    val builder = StreamsBuilder()
 
     @PostConstruct
     fun run(){
-        val input: KStream<String, String> = builder.stream("materializedsimple_input", Consumed.with(Serdes.String(), Serdes.String()))
+        val builder = StreamsBuilder()
+        val ks0: KStream<String, String> = builder.stream("materializedsimple_input", Consumed.with(Serdes.String(), Serdes.String()))
 
-        val aggregated = input
+        val aggregated: KTable<String, Long> = ks0
                 .mapValues { textLine -> textLine.toLowerCase() }
                 .flatMapValues { loweredCase -> loweredCase.split(" ") }
                 .selectKey { key, word -> word }
-                .groupBy{key: String, value: String -> value}
+                .groupBy{ key: String, value: String -> value}
                 .count(Materialized.`as`("mycount"))
                 .filter { key: String, value: Long ->  key == "pink" } // this doesnt work????, why>??
 
+        val mystore: StoreBuilder<KeyValueStore<String, Long>> = Stores.keyValueStoreBuilder(
+                Stores.inMemoryKeyValueStore("mystore"),
+                Serdes.StringSerde(),
+                Serdes.LongSerde()
+        )
+
+        builder.addStateStore(mystore)
+//        aggregated.transformValues({MyTransformer()}, "mystore", "asdf")
 
         aggregated.toStream()
                 // we have a windowed key, so we cannot send this directly to another topic. Cause of that we map to set the real key of the window
@@ -43,7 +72,8 @@ class MaterializedSimple {
                 .to("materializedsimple_output", Produced.with(Serdes.StringSerde(), Serdes.Long()))
 
         // start
-        val streams = KafkaStreams(builder.build(), prop)
+        val topology: Topology = builder.build()
+        val streams = KafkaStreams(topology, this.getProps())
         streams.start()
         Runtime.getRuntime().addShutdownHook(Thread(streams::close))
     }
