6adacd13b7c4a54fb5e1ce7b23020c43234d5b83
tatitati <francis.albusac@simplybusiness.co.uk>
2020-10-25 16:59:48 +0000

    git s

--- subprojects/infrastructure/src/main/kotlin/kafkastream/store/MaterializedSimple.kt
+++ subprojects/infrastructure/src/main/kotlin/kafkastream/store/MaterializedSimple.kt
@@ -1,13 +1,18 @@
 package myapp.infrastructure.kafkastream.store
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import myapp.infrastructure.kafkastream.pojos.Person
 import myapp.infrastructure.kafkastream.serdes.SerdesPerson
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.StreamsConfig
-import org.apache.kafka.streams.kstream.Consumed
-import org.apache.kafka.streams.kstream.KStream
-import org.apache.kafka.streams.kstream.ValueTransformer
+import org.apache.kafka.streams.kstream.*
 import org.apache.kafka.streams.processor.ProcessorContext
 import org.apache.kafka.streams.state.KeyValueStore
 import org.apache.kafka.streams.state.StoreBuilder
@@ -23,32 +28,62 @@ import javax.annotation.PostConstruct
 @SpringBootApplication
 class MaterializedSimple {
 
-    private fun getProps(): Properties {
-        return Properties().apply {
-            put(StreamsConfig.APPLICATION_ID_CONFIG, "materialized-application")
-            put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-            put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde())
-            put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.LongSerde())
-        }
+    val builder = StreamsBuilder()
+    val prop = Properties().apply {
+        put(StreamsConfig.APPLICATION_ID_CONFIG, "materialized-application")
+        put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094")
+
     }
+
+    fun sendToTopicSomeUsers(){
+        val jsonMapper = ObjectMapper().apply {
+            registerKotlinModule()
+        }
+
+        val properties = Properties().apply{
+            put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094")
+            put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+            put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+        }
+
+        val producer = KafkaProducer<String, String>(properties)
+
+        for(i in 1..10) {
+            val person = Person(
+                    firstName = "firstname",
+                    lastName = "lastName",
+                    age = 34
+            )
+            val futureResult = producer.send(ProducerRecord(
+                    "kstream_inputXX",
+                    jsonMapper.writeValueAsString(person)
+            ))
+            futureResult.get()
+        }
+
+        producer.flush()
+        producer.close()
+    }
+
 
     @PostConstruct
     fun run(){
-        val builder = StreamsBuilder()
-        val ks0: KStream<String, Person> = builder.stream("materializedsimple_input", Consumed.with(Serdes.String(), SerdesPerson()))
+        this.sendToTopicSomeUsers()
 
-        val ksowithKey: KStream<String, Person> = ks0
-                .selectKey { key, person -> person.firstName }
+        val serdesSource: Consumed<String, Person>   = Consumed.with(Serdes.String(), SerdesPerson())
+        val serdesSink: Produced<String, Person> = Produced.with(Serdes.String(), SerdesPerson())=
 
-        // create store
-        val mystore: StoreBuilder<KeyValueStore<String, Long>> = Stores.keyValueStoreBuilder(
+        val ks0: KStream<String, Person> = builder.stream("materializedsimple_input", serdesSource)
+        val ks0WithKey: KStream<String, Person> = ks0.selectKey { key, person -> person.firstName }
+
+        // CREATE STORE
+        val mystore: StoreBuilder<KeyValueStore<String, Int>> = Stores.keyValueStoreBuilder(
                 Stores.inMemoryKeyValueStore("mystore"),
-                Serdes.StringSerde(), Serdes.LongSerde())
-
+                Serdes.StringSerde(), Serdes.IntegerSerde())
         builder.addStateStore(mystore)
 
 
-//        ksowithKey.transform()
+        ks0WithKey.transformValues(ValueTransformerSupplier { MyValueTransformer() })
 
 
 //        ksowithKey.toStream()
@@ -62,6 +97,22 @@ class MaterializedSimple {
 //        streams.start()
 //        Runtime.getRuntime().addShutdownHook(Thread(streams::close))
     }
+}
+
+class MyValueTransformer: ValueTransformer<Person, KeyValue<String, Person>> {
+    override fun init(context: ProcessorContext?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun transform(value: Person?): KeyValue<String, Person> {
+        TODO("Not yet implemented")
+    }
+
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+
+
 }
 
 fun main(args: Array<String>) {
