302ea402b2dd7d5d65df3b537221bfad57d979e6
tatitati <francis.albusac@simplybusiness.co.uk>
2020-10-27 18:55:13 +0000

    asdf

--- subprojects/domain/src/test/kotlin/Faker.kt
+++ subprojects/domain/src/test/kotlin/Faker.kt
@@ -6,42 +6,51 @@ import java.util.*
 
 class Faker {
     companion object {
-        fun anyBoolean(): Boolean  = anyOf(listOf(true, false))
-
-        fun anyUUID(): UUID = UUID.randomUUID()
-
         fun anyInt(min: Int = -100000, max: Int = 100000): Int = (min..max).random()
         fun anyIntPositive(max: Int = 100000): Int = anyInt(min = 0, max = max)
-        fun anyIntNegative(min: Int = -100000): Int = anyInt(min = min, max = 0)
 
-        fun<T> anyOf(items: Collection<T>): T {
+        fun <T> anyOf(items: Collection<T>): T {
             val size = items.size
             val randomindex = anyIntPositive(size - 1)
             return items.elementAt(randomindex)
         }
 
-        fun anyString(maxLength: Int = 30): String {
-            val charactersPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9') + listOf('.', '_', '-', ' ', '#', '!', '/', '\\')
-            val randomLength = anyInt(min = 2, max = maxLength)
+        fun <T> anyListFromBuilder(builder: () -> T, withMinLength: Int = 0, withMaxLength: Int = 10): List<T> {
+            val amountItems = anyInt(min = withMinLength, max = withMaxLength)
 
-            val randomTextAccumulator = ""
-            (0..randomLength).forEach{
-                randomTextAccumulator + anyOf(charactersPool)
+            val items = mutableListOf<T>()
+            for (i in 1..amountItems) {
+                items.add(builder())
+            }
+
+            return items.toList()
+        }
+
+        fun anyString(withMaxLength: Int = 30, withMinLength: Int = 3, withCharactersPool: List<Char>? = null, allowEmpty: Boolean = false): String {
+            val charactersPool: List<Char> = withCharactersPool
+                    ?: ('a'..'z') + ('A'..'Z') + ('0'..'9') + listOf('.', '_', '-', ' ', '#', '!', '/', '\\')
+            val randomLength = anyInt(min = withMinLength, max = withMaxLength)
+
+            var randomTextAccumulator = ""
+            (0..randomLength).forEach {
+                randomTextAccumulator += anyOf(charactersPool)
+            }
+
+            if (allowEmpty) {
+                return anyOf(listOf("", randomTextAccumulator))
             }
 
             return randomTextAccumulator
         }
 
-        fun anyDateInPast(before: Date? = null): Date {
-            var now = Instant.now()
-            return Date.from(now.minus(Duration.ofDays(anyIntPositive(max = 1000) as Long)))
+        fun anyTopicName(): String {
+            val charactersPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9') + listOf('.', '_', '-')
+            return anyString(withMinLength = 5, withCharactersPool = charactersPool)
         }
 
-        fun anyDateInFuture(after: Date? = null): Date {
-            var now = Instant.now()
-            return Date.from(now.plus(Duration.ofDays(anyIntPositive(max = 1000) as Long)))
+        fun anyWord(allowEmpty: Boolean = false): String {
+            val charactersPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+            return anyString(withMinLength = 4, withMaxLength = 30, withCharactersPool = charactersPool, allowEmpty = allowEmpty)
         }
-
-        fun anyDate(): Date = anyOf(listOf(anyDateInPast(), Date(), anyDateInFuture()))
     }
 }
