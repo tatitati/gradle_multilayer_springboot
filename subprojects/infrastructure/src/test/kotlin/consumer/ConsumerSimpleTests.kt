6375cff2357ecd3f998c54dd20caa3c6916b38a9
tatitati <francis.albusac@simplybusiness.co.uk>
2020-10-28 10:40:02 +0000

    more

---
+++ subprojects/infrastructure/src/test/kotlin/consumer/ConsumerSimpleTests.kt
@@ -0,0 +1,79 @@
+package myapp.test.infrastructure.consumer
+
+import myapp.test.domain.Faker
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+import org.junit.jupiter.api.Test
+import java.time.Duration
+import java.util.*
+
+class ConsumerSimpleTests {
+    val topicInput = "topic_inputSimple"
+
+
+    // FIXTURES
+    fun fixtures(topic: String){
+        val properties = Properties().apply{
+            put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+            put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+            put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+        }
+
+        val producer = KafkaProducer<String, String>(properties)
+
+        for (i in 1..20) {
+            val futureResult = producer.send(ProducerRecord(
+                    topicInput,
+                    "${Faker.anyWord()}"
+            ))
+            futureResult.get()
+        }
+
+        producer.flush()
+        producer.close()
+    }
+
+    fun buildConsumer(): KafkaConsumer<String, String> {
+        val properties = Properties().apply{
+            put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+            put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+            put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+            put(ConsumerConfig.GROUP_ID_CONFIG, "fgg")
+            put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "5")
+        }
+
+        return KafkaConsumer<String, String>(properties)
+    }
+
+    fun consume(consumer: KafkaConsumer<String, String>, max: Int = 5){
+        val results = mutableListOf<ConsumerRecord<String, String>>()
+
+        while(results.size < max){
+            val records: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(1))
+            records.forEach{ record ->
+                val topic: String  = record.topic()
+                val msg: String = record.value()
+                val offset: Long = record.offset()
+
+                println("[$offset] $topic -- $msg")
+                results.add(record)
+            }
+        }
+    }
+
+    @Test
+    fun run(){
+        val consumer = buildConsumer()
+
+        this.fixtures(topicInput)
+        consumer.subscribe(listOf(topicInput))
+        this.consume(consumer, max = 5)
+
+    }
+}
